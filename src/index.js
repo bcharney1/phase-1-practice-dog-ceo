c84c7623a6a56ccaa71a208b5977ee66fb7baf4a
Boris Charney <boris.charney@gmail.com>
2022-10-27 11:12:44 -0700

    commit tree 13876fb01e0d392aa01d72620f6b9462e156d4a4

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+function fetchDogs() {
+  return fetch(imgUrl)
+  .then(response => response.json())
+  .then(result => {
+    dogImages(result.message)
+  })
+}
+
+function dogImages(dogs){
+  console.log(dogs)
+  return dogs.forEach(dog => {
+    console.log(dog)
+    const goodBoyContainer = document.querySelector('#dog-image-container')
+    const goodBoy = document.createElement('img')
+    goodBoy.src = dog
+    goodBoyContainer.appendChild(goodBoy)
+  });
+}
+
